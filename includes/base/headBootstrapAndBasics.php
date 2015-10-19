95ffc04c796f396ef164ef85580d869c7a99ea29
Mikhail11 <optimisist@gmail.com>
2015-10-19 13:03:58 +0300

    Revert "Revert "пробная версия галереи""
    This reverts commit 0468f01fec10d16d16d2abb3bee90542dd0e809f.

--- includes/base/headBootstrapAndBasics.php
+++ includes/base/headBootstrapAndBasics.php
@@ -16,6 +16,10 @@
 <!-- Main style -->
 <link rel="stylesheet" href="includes/css/style.css">
 
+<!-- Gallery style -->
+<link rel="stylesheet" type="text/css" href="includes/css/demo.css" />
+<link rel="stylesheet" type="text/css" href="includes/css/style.css" />
+<link rel="stylesheet" type="text/css" href="includes/css/elastislide.css" />
 <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 <!--[if lt IE 9]>
 	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
